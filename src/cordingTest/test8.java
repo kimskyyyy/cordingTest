e818e29f04734f53609cb546d31772f5282c8d63
kimskyyyy <hhhhh823@naver.com>
2022-08-08 00:10:25 +0900

    7, 8번 진행

--- src/cordingTest/test8.java
+++ src/cordingTest/test8.java
@@ -11,5 +11,18 @@ public class test8 {
        
        sc.close();
        }
+	
+	public double solution(int[] arr) {
+        double answer = 0;
+        double sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        answer = sum /arr.length; 
+        return answer;
+    }
 
 }
+
+//평균 구하기
+//정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
